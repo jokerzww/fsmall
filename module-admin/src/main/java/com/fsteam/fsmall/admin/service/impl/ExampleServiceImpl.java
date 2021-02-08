af12d0d67221227b8ff9250e95cf4d8986a436a5
TheJokerW <36848313+TheJokerW@users.noreply.github.com>
2021-02-08 19:06:02 +0800

    修复mbg更新 引发的项目文件修改问题

--- module-admin/src/main/java/com/fsteam/fsmall/admin/service/impl/ExampleServiceImpl.java
+++ module-admin/src/main/java/com/fsteam/fsmall/admin/service/impl/ExampleServiceImpl.java
@@ -1,17 +1,9 @@
 package com.fsteam.fsmall.admin.service.impl;
 
-import com.alibaba.druid.support.hibernate.DruidConnectionProvider;
 import com.fsteam.fsmall.admin.service.ExampleService;
-import com.fsteam.fsmall.common.util.DBUtil;
-import com.fsteam.fsmall.mapper.MallTbDynamicSqlSupport;
-import com.fsteam.fsmall.mapper.MallTbMapper;
-import com.fsteam.fsmall.model.MallTb;
-import org.apache.ibatis.annotations.Mapper;
-import org.mybatis.dynamic.sql.BindableColumn;
-import org.mybatis.dynamic.sql.select.QueryExpressionDSL;
-import org.mybatis.dynamic.sql.select.SelectDSLCompleter;
-import org.mybatis.dynamic.sql.select.SelectModel;
-import org.mybatis.dynamic.sql.util.Buildable;
+import com.fsteam.fsmall.mapper.MallDynamicSqlSupport;
+import com.fsteam.fsmall.mapper.MallMapper;
+import com.fsteam.fsmall.model.Mall;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,18 +13,18 @@ import static org.mybatis.dynamic.sql.SqlBuilder.*;
 @Service
 public class ExampleServiceImpl implements ExampleService {
     @Autowired
-    MallTbMapper mallTbMapper;
+    MallMapper mallMapper;
 
 
 
     public String getMallName(String mallID){
 
         //对应sql 为 select id where name=${mallID} 只选一个的
-        Optional<MallTb> mallTb = mallTbMapper.selectOne(c ->
-                c.where(MallTbDynamicSqlSupport.id, isEqualTo(mallID))
+        Optional<Mall> mall = mallMapper.selectOne(c ->
+                c.where(MallDynamicSqlSupport.id, isEqualTo(mallID))
         );
-        if (mallTb.isPresent()){
-        return mallTb.get().getName();
+        if (mall.isPresent()){
+        return mall.get().getName();
         }else{
             return "null";
         }
