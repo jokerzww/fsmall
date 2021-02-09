6cdd77d4d4e675d8bdadfd9844438732a3e5031f
kaidong <kaidong.chen@kaidong.chen>
2021-02-10 02:21:14 +0800

    完成admin模块的雏形，添加mybatis配置

--- module-mbg/src/test/java/MallTest.java
+++ module-mbg/src/test/java/MallTest.java
@@ -1,97 +1,128 @@
 import com.fsteam.fsmall.mapper.MallDynamicSqlSupport;
 import com.fsteam.fsmall.mapper.MallMapper;
 import com.fsteam.fsmall.model.Mall;
-import com.mysql.cj.xdevapi.SelectStatement;
-import org.apache.ibatis.datasource.unpooled.UnpooledDataSource;
-import org.apache.ibatis.mapping.Environment;
-import org.apache.ibatis.session.Configuration;
-import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.ibatis.session.SqlSessionFactoryBuilder;
-import org.apache.ibatis.transaction.jdbc.JdbcTransactionFactory;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.mybatis.dynamic.sql.SqlBuilder;
-import org.mybatis.dynamic.sql.SqlTable;
+import com.fsteam.fsmall.utils.MyBatisUtils;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.mybatis.dynamic.sql.insert.InsertDSL;
+import org.mybatis.dynamic.sql.insert.render.GeneralInsertStatementProvider;
 import org.mybatis.dynamic.sql.insert.render.InsertStatementProvider;
 import org.mybatis.dynamic.sql.render.RenderingStrategies;
 import org.mybatis.dynamic.sql.select.render.SelectStatementProvider;
+import org.mybatis.dynamic.sql.update.render.UpdateStatementProvider;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import static com.fsteam.fsmall.mapper.MallDynamicSqlSupport.*;
 import static org.mybatis.dynamic.sql.SqlBuilder.*;
 
-
-
-
+@SpringBootTest
 public class MallTest {
-    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/mall_db?useUnicode=true&characterEncoding=utf-8&serverTimezone=UTC&useSSL=false";
-    private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
-    private static final String username = "root";
-    private static final String password = "123456";
+    private MallMapper mallMapper;
 
-    private SqlSessionFactory sqlSessionFactory;
+    @Before
+    public void init() {
+        MyBatisUtils.startSqlSession();
+        mallMapper = MyBatisUtils.getMapper(MallMapper.class);
+    }
 
-    @BeforeEach
-    public void init() throws Exception{
-        Class.forName(JDBC_DRIVER);
-        UnpooledDataSource ds = new UnpooledDataSource(JDBC_DRIVER, JDBC_URL, username, password);
-        Environment environment = new Environment("test", new JdbcTransactionFactory(), ds);
-        Configuration config = new Configuration(environment);
-        config.addMapper(MallMapper.class);
-        sqlSessionFactory = new SqlSessionFactoryBuilder().build(config);
+    @After
+    public void release() {
+        MyBatisUtils.close();
     }
 
     @Test
-    public void testInsert() {
-        try (SqlSession sqlSession = sqlSessionFactory.openSession()) {
-            MallMapper mapper = sqlSession.getMapper(MallMapper.class);
+    public void testInsertMall() {
+        Mall mall = new Mall();
+        mall.setId("shuiguodian5.0");
+        mall.setName("水果店");
+        mall.setDescription("水果");
+        mall.setPhone("13733333333");
+        mall.setAddress("广州增城");
 
-            Mall mall = new Mall();
-            mall.setId("shuiguodian");
-            mall.setName("水果店");
-            mall.setDescription("水果");
-            mall.setPhone("13733333333");
-            mall.setAddress("广州增城");
+        InsertStatementProvider<Mall> insertStatementProvider = insert(mall)
+                .into(new MallDynamicSqlSupport.Mall())
+                .map(id).toProperty("id")
+                .map(name).toProperty("name")
+                .map(description).toProperty("description")
+                .map(phone).toProperty("phone")
+                .map(address).toProperty("address")
+                .build().render(RenderingStrategies.MYBATIS3);
 
-            InsertStatementProvider<Mall> insertStatementProvider = insert(mall)
-                    .into(new MallDynamicSqlSupport.Mall())
-                    .map(MallDynamicSqlSupport.id).toProperty("id")
-                    .map(MallDynamicSqlSupport.name).toProperty("name")
-                    .map(MallDynamicSqlSupport.description).toProperty("description")
-                    .map(MallDynamicSqlSupport.phone).toProperty("phone")
-                    .map(MallDynamicSqlSupport.address).toProperty("address")
-                    .build().render(RenderingStrategies.MYBATIS3);
-
-            int rows = mapper.insert(insertStatementProvider);
-            System.out.println(rows);
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        int rows = mallMapper.insert(insertStatementProvider);
+        System.out.println(rows);
     }
 
     @Test
-    public void testSelect() {
-        try (SqlSession sqlSession = sqlSessionFactory.openSession()) {
-            MallMapper mapper = sqlSession.getMapper(MallMapper.class);
+    public void testGetMall() {
+        SelectStatementProvider selectStatementProvider = select(
+                MallDynamicSqlSupport.id,
+                MallDynamicSqlSupport.name,
+                MallDynamicSqlSupport.description,
+                MallDynamicSqlSupport.description,
+                MallDynamicSqlSupport.phone,
+                MallDynamicSqlSupport.address,
+                MallDynamicSqlSupport.status
+        ).from(new MallDynamicSqlSupport.Mall()).build().render(RenderingStrategies.MYBATIS3);
 
-            SelectStatementProvider selectStatementProvider = select(
-                    MallDynamicSqlSupport.id,
-                    MallDynamicSqlSupport.name,
-                    MallDynamicSqlSupport.description,
-                    MallDynamicSqlSupport.description,
-                    MallDynamicSqlSupport.phone,
-                    MallDynamicSqlSupport.address,
-                    MallDynamicSqlSupport.status
-                    ).from(new MallDynamicSqlSupport.Mall()).build().render(RenderingStrategies.MYBATIS3);
+        List<Mall> malls = mallMapper.selectMany(selectStatementProvider);
+        System.out.println("===============================");
+        System.out.println(malls);
+        System.out.println("===============================");
+    }
 
-            List<Mall> malls = mapper.selectMany(selectStatementProvider);
-            System.out.println("===============================");
-            System.out.println(malls);
-            System.out.println("===============================");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    @Test
+    public void testGenInsert() {
+        Mall record = new Mall();
+        record.setId("shuiguodian50");
+        record.setName("水果店");
+        record.setDescription("水果");
+        record.setPhone("13733333333");
+        record.setAddress("广州增城");
+
+
+        GeneralInsertStatementProvider insertStatement = insertInto(mall)
+                .set(id).toValueWhenPresent(record::getId)
+                .set(name).toValueWhenPresent(record::getName)
+                .set(description).toValueWhenPresent(record::getDescription)
+                .set(phone).toValueWhenPresent(record::getPhone)
+                .set(address).toValueWhenPresent(record::getAddress)
+                .set(iconId).toValueWhenPresent(record::getIconId)
+                .set(status).toValueWhenPresent(record::getStatus)
+                .build()
+                .render(RenderingStrategies.MYBATIS3);
+        System.out.println(insertStatement.getInsertStatement());
+
+        System.out.println(mallMapper.generalInsert(insertStatement));
+    }
+
+    @Test
+    public void testUpdateMall() {
+        Mall record = new Mall();
+        record.setId("shuiguodian50");
+        record.setAddress("广州,增城");
+        record.setStatus(1);
+
+        UpdateStatementProvider updateStatement = update(mall)
+                .set(name).equalToWhenPresent(record::getName)
+                .set(description).equalToWhenPresent(record::getDescription)
+                .set(phone).equalToWhenPresent(record::getPhone)
+                .set(address).equalToWhenPresent(record::getAddress)
+                .set(iconId).equalToWhenPresent(record::getIconId)
+                .set(status).equalToWhenPresent(record::getStatus)
+                .where(id, isEqualTo(record.getId()))
+                .build()
+                .render(RenderingStrategies.MYBATIS3);
+        System.out.println(updateStatement.getUpdateStatement());
+        System.out.println(mallMapper.update(updateStatement));
+    }
+
+    @Test
+    public void testDelete() {
+        System.out.println(mallMapper.deleteByPrimaryKey("benji"));
     }
 }
+
